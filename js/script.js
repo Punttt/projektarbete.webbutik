04fae2093a40399532cf8fa5aeafd0ebc11e1389
Pontus Johansson <johanssonp998@gmail.com>
2026-01-02 13:42:30 +0100

    lagt till en fil för javascript, samt lagt till en bekräftelse för bokning.

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Bokningsbekräftelse
+
+const form = document.querySelector(".bokning-action form");
+const modul = document.querySelector("#booking-modul");
+const closeBtn = document.querySelector("#close-modul");
+
+// När man trycker på boka knapp
+
+form.addEventListener("submit", function (e){
+    e.preventDefault();
+    modul.classList.remove("hidden");
+});
+
+// När man klickar på stäng-knappen
+
+closeBtn.addEventListener("click", function (){
+    modul.classList.add("hidden");
+})
